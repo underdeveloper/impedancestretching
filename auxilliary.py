e750195a4d24588feabeae448734eebf8ceb3acd
underdeveloper <der.sulthana@gmail.com>
2021-12-04 12:34:27 +0700

    Rotation towards generator is clockwise...

--- auxilliary.py
+++ auxilliary.py
@@ -13,11 +13,11 @@ def find_reflection_coef(Z: complex, Zo: complex):
     return (Z-Zo)/(Z+Zo)
 
 def find_point_impedance(G: complex, Zo: complex):
-    return -1*Zo*(G+1)/(G-1)
+    return Zo*(G+1)/(1-G)
 
 def rotate_on_smith_chart(G: complex, towards_generator: bool, distance_in_lambdas: float):
     direction = 1 if towards_generator else -1
-    angle = (distance_in_lambdas / (0.5*np.pi)) * 2*np.pi
-    G_prime = G * np.exp(direction*1j*angle)
+    angle = (distance_in_lambdas / (0.5)) * 2*np.pi
+    G_prime = G * np.exp(direction*-1j*angle)
     G_prime_rounded = round(np.real(G_prime),6) + 1j * round(np.imag(G_prime),6)
     return G_prime_rounded
